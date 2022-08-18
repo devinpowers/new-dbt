5e5f2bb03106df63ab3ab2dc74d33e904ba3a083
Devin Powers <devinjpowers@gmail.com>
2022-08-18 19:21:47 +0000

    new updates

--- models/staging/jaffle_shop/stg_customers.sql
+++ models/staging/jaffle_shop/stg_customers.sql
@@ -1,11 +1,8 @@
-with customers as (
-    
-    select 
-        id as customer_id,
-        first_name,
-        last_name
+select 
+    id as customer_id,
+    first_name,
+    last_name
+from {{ source('jaffle_shop', 'customers') }}
 
-    from raw.jaffle_shop.customers
-)
 
-select * from customers
+
