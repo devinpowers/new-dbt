2c785043f515054e5b925016e882eee5dba09c5e
Devin Powers <devinjpowers@gmail.com>
2022-08-21 14:15:47 +0000

    update, remove freshness

--- models/staging/jaffle_shop/stg_customers.sql
+++ models/staging/jaffle_shop/stg_customers.sql
@@ -2,7 +2,7 @@ select
     id as customer_id,
     first_name,
     last_name
-from {{ source('jaffle_shop', 'customers') }}
+from {{ source('jaffle_shop', 'customers') }} -- where to look for data
 
 
 
